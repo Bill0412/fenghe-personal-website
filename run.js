a3a7a06d5a190bceed0766203e0c66bc33401337
Bill Xu <jxphxufh@gmail.com>
2018-07-24 01:06:58 +0800

    fix: try to lisent on port 80

--- run.js
+++ run.js
@@ -9,6 +9,6 @@ const server = http.createServer((req, res) => {
   res.end('Hello World!\n');
 });
 
-server.listen(port, hostname, () => {
+server.listen(hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
