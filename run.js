b751f628b6d57351d110984847e67f3f899bcde8
Bill Xu <jxphxufh@gmail.com>
2018-07-24 01:49:07 +0800

    feat: support loading index.html

--- run.js
+++ run.js
@@ -1,12 +1,21 @@
 const http = require('http');
+const fs = require('fs')
+
+let index = '';
+fs.readFile('index.html', 'utf8', (err, data) => {
+  index = data;
+})
 
 const hostname = '0.0.0.0';
 const port = 80;
 
+// const hostname = '127.0.0.1';
+// const port = 8080;
+
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World!\n');
+  res.setHeader('Content-Type', 'text/html');
+  res.end(index);
 });
 
 server.listen(port, hostname, () => {
